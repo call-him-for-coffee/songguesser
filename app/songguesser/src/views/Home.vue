1778efa10eaeab10143c3facf95b4dbc1ca3c388
Derlie Monne <derlie.monne@gmail.com>
2024-02-04 20:03:57 +1000

    feat(app): logout

--- app/songguesser/src/views/Home.vue
+++ app/songguesser/src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
-  <div class="title">song guesser</div>
-  <div class="container">
-    <div class="row">
-
-      <div class="col-md-offset-3 col-md-6">
-        <span class="heading">здесь будет сама игра...</span>          
+  <div class="row">
+    <div class="col-md-offset-3 col-md-6">
+      <div class="flex-container">
+        <span class="name">{{ auth_username }}</span>
+        <button class="sex-button" @click="onExitClick">Logout</button>
       </div>
-
     </div>
   </div>
 </template>
   
 
-  
+<script>
+import { HTTP } from '../scripts/api';
+
+export default {
+  data() {
+    return {
+      auth_username: '',
+    }
+  },
+  methods: {
+    onExitClick() {
+      console.log("onExitClick")
+      localStorage.removeItem("username")
+      localStorage.removeItem("token")
+      localStorage.removeItem("user_id")
+      this.$router.push('/login')
+    }
+  },
+  beforeMount() {
+    console.log("beforeMount")
+    this.auth_username = localStorage.getItem("username")
+  }
+};
+
+
+
+</script>
+
   
 <style src="../stylesheets/mathsex.css"></style>
   
